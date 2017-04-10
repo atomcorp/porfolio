6a03dd1f1405e572541170966ac2b610db08fc26
atomcorp <thomasmaxwellsmith@gmail.com>
2017-04-10 21:17:35 +0100

    bit more interesting, adding a border on container and fiddling w/ values

--- source/js/content/skills.js
+++ source/js/content/skills.js
@@ -1,7 +1,8 @@
 var skills = function() {
 
 	var dom = {
-		skill: document.querySelectorAll('.skill')
+		skill: document.querySelectorAll('.skill'),
+		container: document.querySelector('.skills__container')
 	};
 
 	var state = {
@@ -15,8 +16,8 @@ var skills = function() {
 		var margin = [];
 		// margins are the set css shorthand top/right/bottom/left
 		// margins must add up to 60 / 60
-		margin.push(Math.floor((Math.random() * 30) + 1)); // top
-		margin.push(Math.floor((Math.random() * 30) + 1)); // right
+		margin.push(Math.floor((Math.random() * 50) + 1)); // top
+		margin.push(Math.floor((Math.random() * 50) + 1)); // right
 		margin.push(30 - margin[0]); // bottom
 		margin.push(30 - margin[1]); // left
 		for (var i = 0; i < margin.length; i++) {
@@ -25,12 +26,13 @@ var skills = function() {
 		return margin;
 	};
 
-	function _getRadius() {
+	function _getRadius(range, min) {
+		// threshold = range, min
 		var radius = [];
-		radius.push(Math.floor((Math.random() * 50) + 51)); // top
-		radius.push(Math.floor((Math.random() * 50) + 51)); // right
-		radius.push(Math.floor((Math.random() * 50) + 51)); // bottom
-		radius.push(Math.floor((Math.random() * 50) + 51)); // left
+		radius.push(Math.floor((Math.random() * range) + min)); // top
+		radius.push(Math.floor((Math.random() * range) + min)); // right
+		radius.push(Math.floor((Math.random() * range) + min)); // bottom
+		radius.push(Math.floor((Math.random() * range) + min)); // left
 		for (var i = 0; i < radius.length; i++) {
 			radius[i] += '%';
 		}
@@ -40,18 +42,29 @@ var skills = function() {
 	function _setMargin() {
 		for (skill of dom.skill) {
 			var margin = _getMargin();
-			var radius = _getRadius();
 			skill.style.margin = margin.join(' ');
-			skill.style['border-radius'] = radius.join(' ');
 		}
 	};
 
-	function runMargin() {
+	function _setRadius() {
+		for (skill of dom.skill) {
+			var radius = _getRadius(50, 51);
+			skill.style['border-radius'] = radius.join(' ');
+		}
+		var containerRadius = _getRadius(20, 20);
+		dom.container.style['border-radius'] = containerRadius.join(' ');
+	};
+
+	function runSkills() {
+		setContainer();
 		_setMargin();
 		if (!state.interval) {
 			state.interval = setInterval (function() {
 				_setMargin();
 			}, 1000);
+			setInterval (function() {
+				_setRadius();
+			}, 500);
 		}
 	};
 
@@ -59,11 +72,26 @@ var skills = function() {
 		clearInterval(state.interval);
 	};
 
+	function setContainer() {
+		// get whatevers more height or width
+		// assign smaller number to the other
+		var width = dom.container.offsetWidth;
+		var height = dom.container.offsetHeight;
+		if (width > height) {
+			dom.container.style.width = height + 'px';
+			dom.container.style.height = height + 'px';
+		} else {
+			dom.container.style.height = width + 'px';
+			dom.container.style.width = width + 'px';
+		}
+	}
+
 	return {
-		runMargin: runMargin,
-		cancelMargin: cancelMargin
+		runSkills: runSkills,
+		cancelMargin: cancelMargin,
+		setContainer: setContainer
 	}
 }
 
 var newSkill = skills();
-// newSkill.runMargin();
+newSkill.runSkills();
