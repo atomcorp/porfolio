bf682fb18893314e86e79f45e061458cf472b5cb
atomcorp <thomasmaxwellsmith@gmail.com>
2017-04-10 20:26:02 +0100

    added moving skills position and borders

--- source/js/content/skills.js
+++ source/js/content/skills.js
@@ -4,38 +4,66 @@ var skills = function() {
 		skill: document.querySelectorAll('.skill')
 	};
 
+	var state = {
+		interval: false
+	};
+
 	// what does this do?
 	// randomly assign different margins to the skills boxes
-
-	// margins must add up to 60 / 60
 
 	function _getMargin() {
 		var margin = [];
 		// margins are the set css shorthand top/right/bottom/left
-		margin.push(Math.floor((Math.random() * 60) + 1)); // top
-		margin.push(Math.floor((Math.random() * 60) + 1)); // right
-		margin.push(60 - margin[0]); // bottom
-		margin.push(60 - margin[0]; // left
+		// margins must add up to 60 / 60
+		margin.push(Math.floor((Math.random() * 30) + 1)); // top
+		margin.push(Math.floor((Math.random() * 30) + 1)); // right
+		margin.push(30 - margin[0]); // bottom
+		margin.push(30 - margin[1]); // left
+		for (var i = 0; i < margin.length; i++) {
+			margin[i] += 'px';
+		}
 		return margin;
-	}
+	};
+
+	function _getRadius() {
+		var radius = [];
+		radius.push(Math.floor((Math.random() * 50) + 51)); // top
+		radius.push(Math.floor((Math.random() * 50) + 51)); // right
+		radius.push(Math.floor((Math.random() * 50) + 51)); // bottom
+		radius.push(Math.floor((Math.random() * 50) + 51)); // left
+		for (var i = 0; i < radius.length; i++) {
+			radius[i] += '%';
+		}
+		return radius;
+	};
 
 	function _setMargin() {
-		console.log(dom.skill);
 		for (skill of dom.skill) {
 			var margin = _getMargin();
-			console.log(margin, skill);
+			var radius = _getRadius();
 			skill.style.margin = margin.join(' ');
+			skill.style['border-radius'] = radius.join(' ');
 		}
-	}
+	};
 
 	function runMargin() {
 		_setMargin();
-	}
+		if (!state.interval) {
+			state.interval = setInterval (function() {
+				_setMargin();
+			}, 1000);
+		}
+	};
+
+	function cancelMargin() {
+		clearInterval(state.interval);
+	};
 
 	return {
-		runMargin: runMargin
+		runMargin: runMargin,
+		cancelMargin: cancelMargin
 	}
 }
 
 var newSkill = skills();
-newSkill.runMargin();
+// newSkill.runMargin();
