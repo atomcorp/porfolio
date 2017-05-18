0fb0a40dcf11cdbed7e28c09a1c86cfa6269059c
atomcorp <thomasmaxwellsmith@gmail.com>
2017-05-18 01:03:14 +0100

    added some browser styling, fiddling with label animations

--- source/js/content/projects.js
+++ source/js/content/projects.js
@@ -2,12 +2,14 @@ const projects = function() {
 
   const dom = {
     images: document.querySelectorAll('.project__images'),
-    projects: document.querySelectorAll('.project')
+    projects: document.querySelectorAll('.project'),
+
   };
 
   let state = {
     activeProjectId: 1,
-    activeProjectIndex: 1
+    isAnimating: false,
+    animationDuration: 1000
   };
 
   function init() {
@@ -25,17 +27,21 @@ const projects = function() {
     for (const project of dom.projects) {
       project.addEventListener('click', function() {
         /* jshint validthis: true */
-        const self = this;  
-        _setActiveProject(self.dataset.projectId);
-        _moveProjectImages();
+        if (state.isAnimating === false) {
+          const self = this;  
+          state.isAnimating = true;
+          _setActiveProject(self.dataset.projectId);
+          _moveProjectImages();
+          _moveProjectLabels();
+          setTimeout(function() {
+            state.isAnimating = false;
+          }, state.animationDuration)
+        }
       }, false);
-    }
-    
+    }  
   }
 
   function _setActiveProject(newId = 1) {
-    // expects a number 
-    state.priorProjectId = state.activeProjectId;
     state.activeProjectId = parseInt(newId);
   }
 
@@ -45,6 +51,26 @@ const projects = function() {
     movement = (state.slideWidth * state.activeProjectId) - state.slideWidth;
     for (const image of dom.images) {
       image.style.transform = `translateX(-${movement}px)`;
+    }
+  }
+
+  function _moveProjectLabels() {
+    for (const project of dom.projects) {
+      const underline = project.querySelector('.project__underline');
+      if (parseInt(project.dataset.projectId) === state.activeProjectId) {
+        if (underline.classList.contains('project__underline--reverse')) {
+          underline.classList.remove('project__underline--reverse');
+        }
+        underline.classList.add('project__underline--animate');
+      } else {
+        if (underline.classList.contains('project__underline--animate')) {
+          underline.classList.remove('project__underline--animate');
+          underline.classList.add('project__underline--reverse');
+          setTimeout(function() {
+            underline.classList.remove('project__underline--reverse');
+          }, state.animationDuration);
+        }
+      }
     }
   }
 
